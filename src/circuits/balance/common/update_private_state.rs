134af8fd57e84ae8d1233c882fc8ce5f30ed8e5a
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-10-07 12:42:17 +0700

    feat: update private state

--- src/circuits/balance/common/update_private_state.rs
+++ src/circuits/balance/common/update_private_state.rs
@@ -1,4 +1,18 @@
-
+use crate::{
+    common::{
+        private_state::{PrivateState, PrivateStateTarget},
+        trees::{
+            asset_tree::{AssetMerkleProof, AssetMerkleProofTarget},
+            nullifier_tree::{NullifierInsertionProof, NullifierInsertionProofTarget},
+        },
+    },
+    constants::ASSET_TREE_HEIGHT,
+    ethereum_types::{
+        bytes32::{Bytes32, Bytes32Target},
+        u32limb_trait::U32LimbTargetTrait as _,
+        u256::{U256, U256Target},
+    },
+};
 use plonky2::{
     field::{extension::Extendable, types::Field},
     hash::hash_types::RichField,
@@ -8,30 +22,12 @@ use plonky2::{
         config::{AlgebraicHasher, GenericConfig},
     },
 };
-use crate::{
-    common::{
-        private_state::{PrivateState, PrivateStateTarget},
-        salt::{ SaltTarget},
-        trees::{
-            asset_tree::{AssetMerkleProof, AssetMerkleProofTarget},
-            nullifier_tree::{NullifierInsertionProof, NullifierInsertionProofTarget},
-        },
-    },
-    constants::ASSET_TREE_HEIGHT,
-    ethereum_types::{
-        bytes32::{Bytes32, Bytes32Target},
-        u256::{U256Target, U256},
-        u32limb_trait::U32LimbTargetTrait as _,
-    },
-};
-
 
 #[derive(Debug, thiserror::Error, Clone)]
-pub enum UpdatePrivateStateError{
+pub enum UpdatePrivateStateError {
     #[error("Merkle proof error: {0}")]
     MerkleProofError(String),
 }
-
 
 #[derive(Debug, Clone)]
 pub struct UpdatePrivateState {
@@ -98,7 +94,6 @@ impl UpdatePrivateState {
     }
 }
 
-
 #[derive(Debug, Clone)]
 pub struct UpdatePrivateStateTarget {
     pub token_index: Target,
@@ -128,50 +123,62 @@ impl UpdatePrivateStateTarget {
         <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
     {
         let token_index = builder.add_virtual_target();
+        if is_checked {
+            builder.range_check(token_index, 32);
+        }
         let amount = U256Target::new(builder, is_checked);
         let nullifier = Bytes32Target::new(builder, is_checked);
-        let new_salt = SaltTarget::new(builder);
         let prev_private_state = PrivateStateTarget::new(builder);
         let nullifier_proof = NullifierInsertionProofTarget::new(builder, is_checked);
-        let prev_asset_leaf = AssetLeafTarget::new(builder, is_checked);
+        let prev_balance = U256Target::new(builder, is_checked);
         let asset_merkle_proof = AssetMerkleProofTarget::new(builder, ASSET_TREE_HEIGHT);
+        let new_private_state = PrivateStateTarget::new(builder);
 
         let prev_private_commitment = prev_private_state.commitment(builder);
         let new_nullifier_tree_root = nullifier_proof.get_new_root::<F, C, D>(
             builder,
-            prev_private_state.nullifier_tree_root,
+            prev_private_state.nullifier_tree_root.clone(),
             nullifier,
         );
+
         asset_merkle_proof.verify::<F, C, D>(
             builder,
-            &prev_asset_leaf,
+            &prev_balance,
             token_index,
-            prev_private_state.asset_tree_root,
+            prev_private_state.asset_tree_root.clone(),
         );
-        let new_asset_leaf = prev_asset_leaf.add(builder, amount);
+
+        let new_asset_leaf = prev_balance.add(builder, &amount);
         let new_asset_tree_root =
             asset_merkle_proof.get_root::<F, C, D>(builder, &new_asset_leaf, token_index);
-        let new_private_state = PrivateStateTarget {
-            asset_tree_root: new_asset_tree_root,
-            nullifier_tree_root: new_nullifier_tree_root,
-            prev_private_commitment,
-            salt: new_salt,
-            ..prev_private_state
-        };
+
+        new_private_state
+            .asset_tree_root
+            .connect(builder, new_asset_tree_root);
+        new_private_state
+            .nullifier_tree_root
+            .connect(builder, new_nullifier_tree_root);
+        new_private_state
+            .prev_private_commitment
+            .connect(builder, prev_private_commitment);
+        builder.connect(new_private_state.nonce, prev_private_state.nonce);
+        new_private_state
+            .salt
+            .connect(builder, prev_private_state.salt);
+
         Self {
             token_index,
             amount,
             nullifier,
-            new_private_state_salt: new_salt,
             prev_private_state,
             nullifier_proof,
-            prev_asset_leaf,
+            prev_balance,
             asset_merkle_proof,
             new_private_state,
         }
     }
 
-    pub fn set_witness<W: WitnessWrite<F>, F: Field>(
+    pub fn set_witness<F: Field, W: WitnessWrite<F>>(
         &self,
         witness: &mut W,
         value: &UpdatePrivateState,
@@ -179,14 +186,11 @@ impl UpdatePrivateStateTarget {
         witness.set_target(self.token_index, F::from_canonical_u32(value.token_index));
         self.amount.set_witness(witness, value.amount);
         self.nullifier.set_witness(witness, value.nullifier);
-        self.new_private_state_salt
-            .set_witness(witness, value.new_private_state_salt);
         self.prev_private_state
             .set_witness(witness, &value.prev_private_state);
         self.nullifier_proof
             .set_witness(witness, &value.nullifier_proof);
-        self.prev_asset_leaf
-            .set_witness(witness, value.prev_asset_leaf);
+        self.prev_balance.set_witness(witness, value.prev_balance);
         self.asset_merkle_proof
             .set_witness(witness, &value.asset_merkle_proof);
         self.new_private_state
@@ -207,7 +211,6 @@ mod tests {
     use rand::Rng;
 
     use crate::{
-        circuits::balance::receive::receive_targets::private_state_transition::PrivateStateTransitionTarget,
         common::{
             private_state::PrivateState,
             salt::Salt,
@@ -219,46 +222,42 @@ mod tests {
         utils::poseidon_hash_out::PoseidonHashOut,
     };
 
-    use super::UpdatePrivateState;
+    use super::{UpdatePrivateState, UpdatePrivateStateTarget};
 
     type F = GoldilocksField;
     type C = PoseidonGoldilocksConfig;
     const D: usize = 2;
 
     #[test]
-    fn test_private_state_transition() {
+    fn test_update_private_state_circuit() {
         let mut rng = rand::thread_rng();
         let transfer = Transfer::rand(&mut rng);
-        let _pubkey = transfer.recipient.to_pubkey().unwrap();
-
         let mut asset_tree = AssetTree::new(ASSET_TREE_HEIGHT);
         let mut nullifier_tree = NullifierTree::new();
         let prev_private_state = PrivateState {
             asset_tree_root: asset_tree.get_root(),
             nullifier_tree_root: nullifier_tree.get_root(),
             prev_private_commitment: PoseidonHashOut::default(),
-            nonce: rng.gen(),
+            nonce: rng.r#gen(),
             salt: Salt::rand(&mut rng),
         };
         let prev_private_commitment = prev_private_state.commitment();
 
         let prev_asset_leaf = asset_tree.get_leaf(transfer.token_index as u64);
         let asset_merkle_proof = asset_tree.prove(transfer.token_index as u64);
-        let new_asset_leaf = prev_asset_leaf.add(transfer.amount);
+        let new_asset_leaf = prev_asset_leaf + transfer.amount;
         asset_tree.update(transfer.token_index as u64, new_asset_leaf);
 
         let nullifier: Bytes32 = transfer.poseidon_hash().into();
         let nullifier_proof = nullifier_tree.prove_and_insert(nullifier).unwrap();
 
-        let new_salt = Salt::rand(&mut rng);
         let value = UpdatePrivateState::new(
             transfer.token_index,
             transfer.amount,
             nullifier,
-            new_salt,
             &prev_private_state,
             &nullifier_proof,
-            &prev_asset_leaf,
+            prev_asset_leaf,
             &asset_merkle_proof,
         )
         .unwrap();
@@ -267,13 +266,12 @@ mod tests {
             asset_tree_root: asset_tree.get_root(),
             nullifier_tree_root: nullifier_tree.get_root(),
             prev_private_commitment,
-            nonce: prev_private_state.nonce,
-            salt: new_salt,
+            ..prev_private_state.clone()
         };
         assert_eq!(value.new_private_state, expected_new_private_state);
 
         let mut builder = CircuitBuilder::new(CircuitConfig::default());
-        let target = PrivateStateTransitionTarget::new::<F, C, D>(&mut builder, true);
+        let target = UpdatePrivateStateTarget::new::<F, C, D>(&mut builder, true);
         let data = builder.build::<C>();
 
         let mut pw = PartialWitness::<F>::new();
