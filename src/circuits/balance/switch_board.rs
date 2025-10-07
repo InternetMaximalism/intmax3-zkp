269dc7b126b8b6665db416c090c6aa399adfcc2c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-10-07 16:11:04 +0700

    feat: wip fix test_balance_switch_board_circuit

--- src/circuits/balance/switch_board.rs
+++ src/circuits/balance/switch_board.rs
@@ -1,10 +1,13 @@
 use plonky2::{
     field::extension::Extendable,
     hash::hash_types::{HashOutTarget, RichField},
-    iop::target::BoolTarget,
+    iop::{
+        target::BoolTarget,
+        witness::{PartialWitness, WitnessWrite},
+    },
     plonk::{
         circuit_builder::CircuitBuilder,
-        circuit_data::{CircuitConfig, VerifierCircuitData, VerifierCircuitTarget},
+        circuit_data::{CircuitConfig, CircuitData, VerifierCircuitData, VerifierCircuitTarget},
         config::{AlgebraicHasher, GenericConfig},
         proof::{ProofWithPublicInputs, ProofWithPublicInputsTarget},
     },
@@ -41,6 +44,9 @@ pub enum BalanceSwitchBoardError {
 
     #[error("Invalid input: {0}")]
     InvalidInput(String),
+
+    #[error("Failed to prove: {0}")]
+    FailedToProve(String),
 }
 
 pub struct BalanceSwichBoard<
@@ -249,16 +255,22 @@ impl<const D: usize> BalanceSwichBoardTarget<D> {
             new_pis2.clone(),
             new_pis3.clone(),
         ];
+
         let candidate_commitments = candidates
             .iter()
-            .map(|pis| HashOutTarget {
-                elements: pis.commitment(builder, balance_config).elements,
+            .map(|pis| pis.commitment(builder, balance_config))
+            .collect::<Vec<_>>();
+
+        let candidate_hash_targets = candidate_commitments
+            .iter()
+            .map(|hash| HashOutTarget {
+                elements: hash.elements,
             })
             .collect::<Vec<_>>();
 
         let selected_commitment = PoseidonHashOutTarget {
             elements: builder
-                .random_access_hash(position, candidate_commitments)
+                .random_access_hash(position, candidate_hash_targets)
                 .elements,
         };
         let new_pis = BalanceFullPublicInputsTarget::new(builder, balance_config);
@@ -276,5 +288,341 @@ impl<const D: usize> BalanceSwichBoardTarget<D> {
             balance_vd,
             new_pis,
         }
+    }
+
+    pub fn set_witness<F, C, W>(
+        &self,
+        witness: &mut W,
+        value: &BalanceSwichBoard<F, C, D>,
+        balance_vd: &VerifierCircuitData<F, C, D>,
+        new_full_pis: &BalanceFullPublicInputs<F, C, D>,
+    ) -> Result<(), BalanceSwitchBoardError>
+    where
+        F: RichField + Extendable<D>,
+        C: GenericConfig<D, F = F>,
+        <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+        W: WitnessWrite<F>,
+    {
+        let flags = [
+            value.initial_value.is_some(),
+            value.receive_transfer_proof.is_some(),
+            value.receive_deposit_proof.is_some(),
+            value.send_tx_proof.is_some(),
+        ];
+        let total = flags.iter().filter(|f| **f).count();
+        if total != 1 {
+            return Err(BalanceSwitchBoardError::InvalidInput(
+                "exactly one of initial value or proofs must be provided".to_string(),
+            ));
+        }
+
+        for (flag, target) in flags.iter().zip(self.one_hot.iter()) {
+            witness.set_bool_target(*target, *flag);
+        }
+
+        if let Some((user_id, salt)) = value.initial_value {
+            self.initial_value.0.set_witness(witness, user_id);
+            self.initial_value.1.set_witness(witness, salt);
+        } else {
+            self.initial_value.0.set_witness(witness, UserId(0));
+            self.initial_value.1.set_witness(witness, Salt::default());
+        }
+
+        if let Some(proof) = &value.receive_transfer_proof {
+            witness.set_proof_with_pis_target(&self.receive_transfer_proof, proof);
+        }
+        if let Some(proof) = &value.receive_deposit_proof {
+            witness.set_proof_with_pis_target(&self.receive_deposit_proof, proof);
+        }
+        if let Some(proof) = &value.send_tx_proof {
+            witness.set_proof_with_pis_target(&self.send_tx_proof, proof);
+        }
+
+        witness.set_verifier_data_target(&self.balance_vd, &balance_vd.verifier_only);
+        self.new_pis.set_witness(witness, new_full_pis);
+
+        Ok(())
+    }
+}
+
+pub struct BalanceSwichBoardCircuit<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+{
+    pub data: CircuitData<F, C, D>,
+    pub balance_vd: VerifierCircuitData<F, C, D>,
+    pub receive_transfer_vd: VerifierCircuitData<F, C, D>,
+    pub receive_deposit_vd: VerifierCircuitData<F, C, D>,
+    pub send_tx_vd: VerifierCircuitData<F, C, D>,
+    pub balance_config: CircuitConfig,
+    pub target: BalanceSwichBoardTarget<D>,
+    pub public_inputs: BalanceFullPublicInputsTarget,
+}
+
+impl<F, C, const D: usize> BalanceSwichBoardCircuit<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F> + 'static,
+    <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+{
+    pub fn new(
+        balance_vd: &VerifierCircuitData<F, C, D>,
+        receive_transfer_vd: &VerifierCircuitData<F, C, D>,
+        receive_deposit_vd: &VerifierCircuitData<F, C, D>,
+        send_tx_vd: &VerifierCircuitData<F, C, D>,
+    ) -> Self {
+        let balance_config = balance_vd.common.config.clone();
+        let mut builder = CircuitBuilder::<F, D>::new(balance_config.clone());
+        let target = BalanceSwichBoardTarget::new::<F, C>(
+            &mut builder,
+            &balance_config,
+            receive_transfer_vd,
+            receive_deposit_vd,
+            send_tx_vd,
+        );
+        let public_inputs = target.new_pis.clone();
+        builder.register_public_inputs(&public_inputs.to_vec(&balance_config));
+        let data = builder.build::<C>();
+
+        Self {
+            data,
+            balance_vd: balance_vd.clone(),
+            receive_transfer_vd: receive_transfer_vd.clone(),
+            receive_deposit_vd: receive_deposit_vd.clone(),
+            send_tx_vd: send_tx_vd.clone(),
+            balance_config,
+            target,
+            public_inputs,
+        }
+    }
+
+    pub fn prove(
+        &self,
+        witness: &BalanceSwichBoard<F, C, D>,
+    ) -> Result<ProofWithPublicInputs<F, C, D>, BalanceSwitchBoardError> {
+        let new_full_pis = witness.to_public_inputs(
+            &self.balance_vd,
+            &self.receive_transfer_vd,
+            &self.receive_deposit_vd,
+            &self.send_tx_vd,
+        )?;
+
+        let mut pw = PartialWitness::<F>::new();
+        self.target
+            .set_witness::<F, C, _>(&mut pw, witness, &self.balance_vd, &new_full_pis)?;
+        self.public_inputs.set_witness(&mut pw, &new_full_pis);
+
+        self.data
+            .prove(pw)
+            .map_err(|e| BalanceSwitchBoardError::FailedToProve(e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        circuits::balance::balance_pis::BalanceFullPublicInputs,
+        common::{
+            block_number::BlockNumber, public_state::PublicState, salt::Salt, user_id::UserId,
+        },
+        utils::{conversion::ToField, poseidon_hash_out::PoseidonHashOut},
+    };
+    use plonky2::{
+        field::goldilocks_field::GoldilocksField,
+        iop::witness::PartialWitness,
+        plonk::{
+            circuit_data::{CircuitConfig, CircuitData},
+            config::PoseidonGoldilocksConfig,
+        },
+    };
+    const D: usize = 2;
+    type F = GoldilocksField;
+    type C = PoseidonGoldilocksConfig;
+
+    fn build_balance_circuit() -> (
+        CircuitData<F, C, D>,
+        BalanceFullPublicInputsTarget,
+        CircuitConfig,
+    ) {
+        let config = CircuitConfig::standard_recursion_config();
+        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        let target = BalanceFullPublicInputsTarget::new(&mut builder, &config);
+        builder.register_public_inputs(&target.to_vec(&config));
+        let data = builder.build::<C>();
+        (data, target, config)
+    }
+
+    fn prove_balance(
+        data: &CircuitData<F, C, D>,
+        target: &BalanceFullPublicInputsTarget,
+        pis: &BalanceFullPublicInputs<F, C, D>,
+    ) -> ProofWithPublicInputs<F, C, D> {
+        let mut pw = PartialWitness::<F>::new();
+        target.set_witness::<F, C, D, _>(&mut pw, pis);
+        data.prove(pw).unwrap()
+    }
+
+    fn make_public_state(block: u64) -> PublicState {
+        let root =
+            PoseidonHashOut::from_u64_slice(&[block + 1, block + 2, block + 3, block + 4]).unwrap();
+        PublicState {
+            block_number: BlockNumber::new(block).unwrap(),
+            account_tree_root: root,
+            deposit_tree_root: root,
+            prev_public_state_root: root,
+        }
+    }
+
+    fn make_balance_full_inputs(
+        user_id: UserId,
+        block_r: u64,
+        commitment_seed: u64,
+        balance_vd: &VerifierCircuitData<F, C, D>,
+    ) -> BalanceFullPublicInputs<F, C, D> {
+        let public_state = make_public_state(block_r + 10);
+        let private_commitment = PoseidonHashOut::from_u64_slice(&[
+            commitment_seed,
+            commitment_seed + 1,
+            commitment_seed + 2,
+            commitment_seed + 3,
+        ])
+        .unwrap();
+        let pis = BalancePublicInputs {
+            user_id,
+            public_state,
+            block_r: BlockNumber::new(block_r).unwrap(),
+            private_commitment,
+        };
+        BalanceFullPublicInputs {
+            pis,
+            vd: balance_vd.verifier_only.clone(),
+        }
+    }
+
+    #[cfg_attr(debug_assertions, ignore = "run with --release")]
+    #[test]
+    fn test_balance_switch_board_circuit() {
+        let (balance_data, balance_target, balance_config) = build_balance_circuit();
+        let balance_vd = balance_data.verifier_data();
+
+        let initial_user = UserId::new(0, 1).unwrap();
+        let initial_salt = Salt::default();
+
+        let transfer_pis = make_balance_full_inputs(UserId::new(0, 2).unwrap(), 5, 10, &balance_vd);
+        let deposit_pis = make_balance_full_inputs(UserId::new(0, 3).unwrap(), 6, 20, &balance_vd);
+        let send_pis = make_balance_full_inputs(UserId::new(0, 4).unwrap(), 7, 30, &balance_vd);
+
+        let transfer_proof = prove_balance(&balance_data, &balance_target, &transfer_pis);
+        let deposit_proof = prove_balance(&balance_data, &balance_target, &deposit_pis);
+        let send_proof = prove_balance(&balance_data, &balance_target, &send_pis);
+
+        let receive_transfer_vd = balance_vd.clone();
+        let receive_deposit_vd = balance_vd.clone();
+        let send_tx_vd = balance_vd.clone();
+
+        let circuit = BalanceSwichBoardCircuit::new(
+            &balance_vd,
+            &receive_transfer_vd,
+            &receive_deposit_vd,
+            &send_tx_vd,
+        );
+
+        // Initial value scenario
+        let witness_initial = BalanceSwichBoard {
+            initial_value: Some((initial_user, initial_salt)),
+            receive_transfer_proof: None,
+            receive_deposit_proof: None,
+            send_tx_proof: None,
+        };
+
+        let proof_initial = circuit.prove(&witness_initial).unwrap();
+        circuit.data.verify(proof_initial.clone()).unwrap();
+        let expected_initial = witness_initial
+            .to_public_inputs(
+                &balance_vd,
+                &receive_transfer_vd,
+                &receive_deposit_vd,
+                &send_tx_vd,
+            )
+            .unwrap();
+        let expected_initial_fields = expected_initial
+            .to_u64_vec(&balance_config)
+            .to_field_vec::<F>();
+        assert_eq!(proof_initial.public_inputs, expected_initial_fields);
+
+        // Receive transfer scenario
+        let witness_receive_transfer = BalanceSwichBoard {
+            initial_value: None,
+            receive_transfer_proof: Some(transfer_proof.clone()),
+            receive_deposit_proof: None,
+            send_tx_proof: None,
+        };
+        let proof_receive_transfer = circuit.prove(&witness_receive_transfer).unwrap();
+        circuit.data.verify(proof_receive_transfer.clone()).unwrap();
+        let expected_receive_transfer = witness_receive_transfer
+            .to_public_inputs(
+                &balance_vd,
+                &receive_transfer_vd,
+                &receive_deposit_vd,
+                &send_tx_vd,
+            )
+            .unwrap();
+        let expected_receive_transfer_fields = expected_receive_transfer
+            .to_u64_vec(&balance_config)
+            .to_field_vec::<F>();
+        assert_eq!(
+            proof_receive_transfer.public_inputs,
+            expected_receive_transfer_fields
+        );
+
+        // Receive deposit scenario
+        let witness_receive_deposit = BalanceSwichBoard {
+            initial_value: None,
+            receive_transfer_proof: None,
+            receive_deposit_proof: Some(deposit_proof.clone()),
+            send_tx_proof: None,
+        };
+        let proof_receive_deposit = circuit.prove(&witness_receive_deposit).unwrap();
+        circuit.data.verify(proof_receive_deposit.clone()).unwrap();
+        let expected_receive_deposit = witness_receive_deposit
+            .to_public_inputs(
+                &balance_vd,
+                &receive_transfer_vd,
+                &receive_deposit_vd,
+                &send_tx_vd,
+            )
+            .unwrap();
+        let expected_receive_deposit_fields = expected_receive_deposit
+            .to_u64_vec(&balance_config)
+            .to_field_vec::<F>();
+        assert_eq!(
+            proof_receive_deposit.public_inputs,
+            expected_receive_deposit_fields
+        );
+
+        // Send tx scenario
+        let witness_send_tx = BalanceSwichBoard {
+            initial_value: None,
+            receive_transfer_proof: None,
+            receive_deposit_proof: None,
+            send_tx_proof: Some(send_proof.clone()),
+        };
+        let proof_send_tx = circuit.prove(&witness_send_tx).unwrap();
+        circuit.data.verify(proof_send_tx.clone()).unwrap();
+        let expected_send_tx = witness_send_tx
+            .to_public_inputs(
+                &balance_vd,
+                &receive_transfer_vd,
+                &receive_deposit_vd,
+                &send_tx_vd,
+            )
+            .unwrap();
+        let expected_send_tx_fields = expected_send_tx
+            .to_u64_vec(&balance_config)
+            .to_field_vec::<F>();
+        assert_eq!(proof_send_tx.public_inputs, expected_send_tx_fields);
     }
 }
