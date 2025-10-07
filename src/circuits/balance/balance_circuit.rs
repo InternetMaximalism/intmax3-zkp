a1a684202a960f3b6ee8b9df2f711539d7c54b8a
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-10-07 16:57:16 +0700

    feat: add balance circuit

---
+++ src/circuits/balance/balance_circuit.rs
@@ -0,0 +1,82 @@
+use plonky2::{
+    field::extension::Extendable,
+    hash::hash_types::RichField,
+    plonk::{
+        circuit_builder::CircuitBuilder,
+        circuit_data::{
+            CircuitConfig, CircuitData, CommonCircuitData, VerifierCircuitData,
+            VerifierCircuitTarget,
+        },
+        config::{AlgebraicHasher, GenericConfig},
+        proof::ProofWithPublicInputsTarget,
+    },
+};
+
+use crate::{
+    circuits::balance::balance_pis::{BALANCE_PUBLIC_INPUTS_LEN, BalanceFullPublicInputsTarget},
+    utils::{
+        cyclic::{add_noop_gates, simple_recursion_circuit_data, vd_vec_len},
+        recursively_verifiable::add_proof_target_and_verify,
+    },
+};
+
+pub struct BalanceCircuit<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+{
+    pub data: CircuitData<F, C, D>,
+    pub switch_proof: ProofWithPublicInputsTarget<D>,
+    pub balance_vd: VerifierCircuitTarget,
+}
+
+impl<F, C, const D: usize> BalanceCircuit<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F> + 'static,
+    <C as GenericConfig<D>>::Hasher: AlgebraicHasher<F>,
+{
+    pub fn new(
+        balance_cd: &CommonCircuitData<F, D>,
+        switch_vd: &VerifierCircuitData<F, C, D>,
+    ) -> Self {
+        let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::default());
+        let switch_proof = add_proof_target_and_verify(switch_vd, &mut builder);
+        let new_balance_pis = BalanceFullPublicInputsTarget::from_pis(
+            &switch_proof.public_inputs,
+            &balance_cd.config,
+        );
+        builder.register_public_inputs(&new_balance_pis.to_vec(&balance_cd.config));
+
+        let (data, success) = builder.try_build_with_options::<C>(true);
+        assert_eq!(
+            data.common,
+            balance_cd.clone(),
+            "Common data mismatch in balance circuit"
+        );
+        assert!(success, "Failed to build balance circuit");
+
+        Self {
+            data,
+            switch_proof,
+            balance_vd: new_balance_pis.vd,
+        }
+    }
+
+    pub fn generate_cd() -> CommonCircuitData<F, D> {
+        let data = simple_recursion_circuit_data::<F, C, D>();
+        let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::default());
+        let proof = builder.add_virtual_proof_with_pis(&data.common);
+        let verifier_data = VerifierCircuitTarget {
+            constants_sigmas_cap: builder.add_virtual_cap(data.common.config.fri_config.cap_height),
+            circuit_digest: builder.add_virtual_hash(),
+        };
+        builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+        // pad degree should be ajusted to fullfill common data equality
+        add_noop_gates(&mut builder, 1 << 12);
+        let mut common = builder.build::<C>().common;
+        common.num_public_inputs = BALANCE_PUBLIC_INPUTS_LEN + vd_vec_len(&common.config);
+        common
+    }
+}
--- src/circuits/balance/mod.rs
+++ src/circuits/balance/mod.rs
@@ -1,3 +1,4 @@
+pub mod balance_circuit;
 pub mod balance_pis;
 pub mod common;
 pub mod receive_deposit_circuit;
